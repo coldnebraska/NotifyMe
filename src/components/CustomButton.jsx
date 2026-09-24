ca2a1af1fe82d7805eb5d847fb958c8b8cf6873d
David Brush <brush.david99@gmail.com>
2026-09-24 08:50:32 -0500

    added a custom button component

---
+++ src/components/CustomButton.jsx
@@ -0,0 +1,12 @@
+import { View, Text, TouchableOpacity } from 'react-native'
+import React from 'react'
+
+const CustomButton = ({ title, handlePress, isLoading }) => {
+  return (
+    <TouchableOpacity onPress={handlePress} activeOpacity={0.7} disabled={isLoading}>
+      <Text>{title}</Text>
+    </TouchableOpacity>
+  )
+}
+
+export default CustomButton
